273fce1ecb035b5b5473ee93b7ae85fe3b4d7c2b
rsliapukhinjs <rsliapukhinjs@gmail.com>
2023-07-14 15:43:27 +0300

    add params

--- src/components/Exercise.tsx
+++ src/components/Exercise.tsx
@@ -4,10 +4,11 @@ import { ProgramContext } from "../App";
 
 const Exercise = () => {
   const { exercisesData } = useContext(ProgramContext);
+  const params = useParams();
 
-  console.log(exercisesData);
+  const currentExercise = exercisesData.find((e) => e.id === Number(params.id));
 
-  return <div className="flex-1 bg-red-500">Test</div>;
+  return <div className="flex-1 bg-red-500">TODO</div>;
 };
 
 export default Exercise;
--- src/components/ExerciseInfo.tsx
+++ src/components/ExerciseInfo.tsx
@@ -4,9 +4,6 @@ import { FetchedProp } from "../types/types";
 import Card from "../utils/Card";
 
 const ExerciseInfo = ({ fetchedExercise }: FetchedProp) => {
-  const [showDescription, setShowDescription] = useState(false);
-  const [showVideo, setShowVideo] = useState(false);
-
   return (
     <Card>
       <h2 className="text-3xl">{fetchedExercise.exercise_name}</h2>
@@ -19,42 +16,6 @@ const ExerciseInfo = ({ fetchedExercise }: FetchedProp) => {
       <button className="inline-block px-4 py-2 bg-teal-500 hover:bg-teal-700 rounded-lg">
         <Link to={`/exercise/${fetchedExercise.id}`}>Show More</Link>
       </button>
-
-      {/* STEPS */}
-      {/* <div className="w-full flex flex-col gap-4">
-        <div className="flex p-4 items-center justify-between border-b-2">
-          <p className="uppercase">Steps</p>
-          <button
-            className="bg-teal-500 px-4 py-2 rounded-xl"
-            onClick={() => setShowDescription(!showDescription)}
-          >
-            {showDescription ? "Hide" : "Show"}
-          </button>
-        </div>
-        {showDescription ? (
-          <ul className="list-decimal px-8 flex flex-col items-start gap-4">
-            {fetchedExercise.steps?.map((s) => (
-              <li>{s}</li>
-            ))}
-          </ul>
-        ) : null}
-      </div> */}
-
-      {/* VIDEO */}
-      {/* <div className="w-full flex flex-col gap-4">
-        <div className="flex p-4 items-center justify-between border-b-2">
-          <p className="uppercase">Steps</p>
-          <button
-            className="bg-teal-500 px-4 py-2 rounded-xl"
-            onClick={() => setShowVideo(!showVideo)}
-          >
-            {showVideo ? "Hide" : "Show"}
-          </button>
-        </div>
-        {fetchedExercise.videoURL && showVideo ? (
-          <video src={fetchedExercise.videoURL[0]} controls />
-        ) : null}
-      </div> */}
     </Card>
   );
 };
